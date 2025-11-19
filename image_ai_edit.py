f068d8256d4fce6430cea5f1ed717f50a2b061d7
ChaOsRul3z <me@chaosrul3z.me>
2025-11-19 11:35:47 +0100

    Refactor image processing script: clean up commented code and improve prompt clarity

--- image_ai_edit.py
+++ image_ai_edit.py
@@ -1,37 +1,32 @@
 import glob
 import os
+from google import genai
+from google.genai import types
+from PIL import Image
 
 path="/home/chaosrul3z/Workspaces/mct/de-onschuldige-photobooth/data/media/camera_original"
 
 list_of_files = glob.glob(f'{path}/*') # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
-print(latest_file)
 
 
-# with open("/home/chaosrul3z/Workspaces/mct/de-onschuldige-photobooth/demofile.txt", "a") as f:
-#   f.write("Now the file has more content!")
 
-# from google import genai
-# from google.genai import types
-# from PIL import Image
+client = genai.Client(api_key="")
 
-# client = genai.Client(api_key="")
+prompt = (
+    "Put him in a purple shirt, sitting at a table in a restaurant."
+)
 
-# prompt = (
-#     "Create a picture of my cat eating a nano-banana in a "
-#     "fancy restaurant under the Gemini constellation",
-# )
+image = Image.open(latest_file)
 
-# image = Image.open("data/media/camera_original/20251119-110501-169730.jpg")
+response = client.models.generate_content(
+    model="gemini-2.5-flash-image",
+    contents=[prompt, image],
+)     
 
-# response = client.models.generate_content(
-#     model="gemini-2.5-flash-image",
-#     contents=[prompt, image],
-# )
-
-# for part in response.parts:
-#     if part.text is not None:
-#         print(part.text)
-#     elif part.inline_data is not None:
-#         image = part.as_image()
-#         image.save("generated_image.png")
+for part in response.parts:
+    if part.text is not None:
+        print(part.text)
+    elif part.inline_data is not None:
+        image = part.as_image()
+        image.save("generated_image.png")
